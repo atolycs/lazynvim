9e43327df4bc26a55715fc3eb6f8d6648462eb30
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-12-18 14:10:18 +0900

    feat: debug environment

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+if vim.env.LAZY_STDPATH then
+  load(vim.fn.system("curl -s https://raw.githubusercontent.com/atolycs/dev-bootstrap/main/nvim/bootstrap.lua"))()
+  require("lazy").setup({})
+else
+  require("config.lazy")
+end
 print("Hello World!")
--- minimal-init.lua
+++ minimal-init.lua
@@ -1,1 +1,2 @@
+vim.env.LAZY_STDPATH = ".tests"
 require("init")
