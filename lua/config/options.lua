95de17fe9f4f643a71c464cd7857c760282382c8
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-13 17:59:54 +0900

    feat: add code

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,5 @@ opt.relativenumber = true
 opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
 
+g.lazyvim_prettier_needs_config = false
+g.lazyvim_cmp = "blink.cmp"
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
+    },
+  },
+}
